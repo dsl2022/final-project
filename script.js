b133ec07cecf8bbc43870f19a10c06b415e5b74e
dsl2055 <job2019tmm@gmail.com>
2022-11-22 18:46:49 -0500

    updated

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const container = document.getElementById("container");
 const button = document.createElement("button");
 button.textContent = "submit";
 container.append(button);
+
+button.addEventListener("click", () => {
+  container.append(makeBall("purple"));
+});
+
+function makeBall(color) {
+  const ball = document.createElement("div");
+  ball.style.height = "5rem";
+  ball.style.width = "5rem";
+  ball.style.background = color;
+  ball.style.borderRadius = "100%";
+  return ball;
+}
